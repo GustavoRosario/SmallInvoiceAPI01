8b1553f0caea1d3e06e5f18970683bff5737e527
Gustavo <grosario015@gmail.com>
2025-03-16 12:22:04 -0400

    Comments Modified
    The comments are modified

--- SmallInvoice.Domain/Ports/Repository/ICustomerRepository.cs
+++ SmallInvoice.Domain/Ports/Repository/ICustomerRepository.cs
@@ -6,12 +6,16 @@ namespace SmallInvoice.Domain.Ports.Repository
     {
         public Task<CustomerResponseDto> CreateCustomer(CreateCustomerDto input);
         public Task<CustomerResponseDto> UpdateCustomer(UpdateCustomerDto input);
+        /// <summary>
+        /// Get all data about customers.
+        /// </summary>
+        /// <returns>A CustomerResponseDto object.</returns>
         public Task<List<CustomerDto>> GetCustomer();
         public Task<CustomerDto> GetCustomerById(Guid id);
         public Task<bool> CustomerExits(string CustomerName);
         public Task<CustomerResponseDto> DeleteCustomerById(Guid Id);
         /// <summary>
-        /// Get Customer Name by id like string.
+        /// Get customer name by id like string.
         /// </summary>
         /// <param name="id">Customer Id</param>
         /// <returns>A string value that represent the Customer Name.</returns>
